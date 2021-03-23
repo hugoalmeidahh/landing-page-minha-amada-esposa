785e880e12b001d940f44ccdbbbf5848ff46c5ea
Hugo Almeida <hugo.almeida@exedio.com.br>
2021-03-23 03:36:28 -0300

    configure project next, tailwind, ts, add images michele

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require("next-pwa");
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = withPWA({
+  // other next config
+  pwa: {
+    dest: "public",
+    disable: !isProd,
+  },
+});
